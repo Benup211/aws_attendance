d89a265d66e69a761f70ad82015a61061affad3e
benup <benup211@gmail.com>
2024-09-29 01:40:26 +0545

    fix:last scan remove when ticket not found

--- client/src/state/scanState.ts
+++ client/src/state/scanState.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
-import axios from 'axios';
-import { toast } from 'react-hot-toast';
+import axios from "axios";
+import { toast } from "react-hot-toast";
 import { IScanStore } from "../types/scan-types";
 
 axios.defaults.withCredentials = true;
@@ -10,31 +10,44 @@ export const useScanStore = create<IScanStore>((set) => ({
     result: {
         name: "",
         email: "",
-        time: new Date()
+        time: new Date(),
     },
     setScan: (scan: boolean) => set({ scan }),
     checkCSV: async (csv: string) => {
         try {
-            const [nameStr, emailStr] = csv.split(',');
-            const name=nameStr.trim();
-            const email=emailStr.trim();
-            const response = await axios.post(`${API_URL}/check_attendance`, {
-                name,
-                email
-            }, {
-                headers: {
-                    'accept': 'application/json',
-                    'Content-Type': 'application/json'
+            const [nameStr, emailStr] = csv.split(",");
+            const name = nameStr.trim();
+            const email = emailStr.trim();
+            const response = await axios.post(
+                `${API_URL}/check_attendance`,
+                {
+                    name,
+                    email,
+                },
+                {
+                    headers: {
+                        accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
                 }
+            );
+            set({
+                result: {
+                    name: name,
+                    email: email,
+                    time: response.data.Time,
+                },
             });
-            set({result:{
-                name:name,
-                email:email,
-                time:response.data.Time,
-            }})
-            toast.success(`Ticket Found for ${email}`)
+            toast.success(`Ticket Found for ${email}`);
         } catch (error) {
-            toast.error('Ticket Not Found');
+            toast.error("Ticket Not Found");
+            set({
+                result: {
+                    name: "",
+                    email: "",
+                    time: new Date(),
+                },
+            });
         }
     },
 }));
